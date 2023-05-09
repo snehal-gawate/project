476b53e834e5969a8dd6ad3b27124364d0d34b49
snehalgawate <laxmanS@purecodesoftware.com>
2023-05-09 22:37:00 +0530

    unneccesaery code

--- src/Components/Styled.jsx
+++ src/Components/Styled.jsx
@@ -69,48 +69,4 @@ import {Box,Typography,ListItemText,Button,Divider} from "@mui/material";
         },
       }));
 
-      export  const styleButton = styled(Button)(() => ({
-        position: 'relative',
-        zIndex: 1,
-        
-        '&::before': {
-          content: '""',
-          position: 'absolute',
-          top: 0,
-          left: '-1px', // compensate for the button border
-          width: 0,
-          height: 0,
-          borderTopWidth: '18px',
-          borderBottomWidth: '18px',
-          borderLeftWidth: '10px',
-          borderTopStyle: 'solid',
-          borderBottomStyle: 'solid',
-          borderLeftStyle: 'solid',
-          borderColor: 'transparent',
-          borderTopColor: "#eeeeee",
-          borderBottomColor: "#eeeeee",
-          backgroundColor:"white",
-          zIndex: -1,
-          
-        },
-        
-        '&::after': {
-          content: '""',
-          position: 'absolute',
-          top: 0,
-          right: '-1px', // compensate for the button border
-          width: 0,
-          height: 0,
-          borderTopWidth: '20px',
-          borderBottomWidth: '20px',
-          borderLeftWidth: '12px',
-          borderTopStyle: 'solid',
-          borderBottomStyle: 'solid',
-          borderLeftStyle: 'solid',
-          borderColor: 'transparent',
-          borderTopColor: "white",
-          borderBottomColor: "white",
-          backgroundColor:"#37474f",
-          zIndex: -1,
-        },
-      }));
+      
